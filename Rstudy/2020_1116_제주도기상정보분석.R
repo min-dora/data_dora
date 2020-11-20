d187a1770e1b47369bfc3299c736797dcc7b65d7
min-dora <alswhd07@gmail.com>
2020-11-20 20:31:02 +0900

    update 20:30

--- Rstudy/2020_1116_제주도기상정보분석.R
+++ Rstudy/2020_1116_제주도기상정보분석.R
@@ -38,18 +38,14 @@ data.gr <- group_by(data.month, 월)
 summarise(data.gr, avg = mean(temp_dist))
 
 data.avg.td <- mutate(data.gr, temp_dist_avg = mean(temp_dist))
-##
-```c
- # 20201120 여기까지 수정
-```
-##
+
 # 봄, 가을에 일교차가 큰 것을 볼 수 있음.
 gp <- ggplot(data = data.avg.td)+geom_line(aes(x = 월, y = temp_dist_avg), group = 1, size = 2)+geom_point(aes(x = 월, y = temp_dist_avg), size = 3, colour = "red")
 ggplotly(gp)
 
 # 불쾌지수 변수 만들기 (Temperature Humidity Index =  '1.8 × 온도 - 0.55 (1 - 습도) (1.8 × 온도 - 26) + 32' )
 # 습도가 퍼센트 이므로 소수로 바꿔주기 => (습도/100)
-data.thi <- data.month %>% mutate(THI = 1.8*평균기온-0.55*(1-습도/100)*(1.8*평균기온-26)+32)
+data.thi <-mutate(data.month, THI = 1.8*평균기온-0.55*(1-습도/100)*(1.8*평균기온-26)+32)
 
 # 불쾌지수 단계 변수 만들기
 data.thi$THI_level <- 
@@ -58,22 +54,29 @@ data.thi$THI_level <-
               ifelse(data.thi$THI>=68 & data.thi$THI<75, "보통", "낮음")))
 
 # 불쾌지수 단계가 매우 높음인 날 골라보기
-data.thi %>% subset(THI_level=="매우높음")
+subset(data.thi, THI_level=="매우높음")
 
 # 불쾌지수 최빈값 구하기 (R은 최빈값을 구하는 함수가 내장되어 있지 않음)
 y <- table(data.thi$THI_level)
 names(y)[which(y == max(y))]
 
 # group_by된 데이터에 달 별 불쾌지수 최빈값 변수 추가
-data.thi %>% group_by(월) %>% summarise(THI_level_count = names(table(THI_level))[which(table(THI_level)==max(table(THI_level)))])
-data.thi.group <- data.thi %>% group_by(월) %>% mutate(THI_level_count = names(table(THI_level))[which(table(THI_level)==max(table(THI_level)))])
+data.thi.gr <- group_by(data.thi, 월)
+
+summarise(data.thi.gr, THI_level_count = names(table(THI_level))[which(table(THI_level)==max(table(THI_level)))])
+
+data.thi.group <- mutate(data.thi.gr, THI_level_count = names(table(THI_level))[which(table(THI_level)==max(table(THI_level)))])
+
 # 변수 타입 확인
 str(data.thi.group)
+
 # THI_level_count 변수를 팩터로 변경 및 level설정
-data.thi.group$THI_level_count <- data.thi.group$THI_level_count %>% factor(levels = c("낮음","보통","높음","매우높음"))
+data.thi.group$THI_level_count <- factor(data.thi.group$THI_level_count, levels = c("낮음","보통","높음","매우높음"))
+str(data.thi.group)
 
 # ggplot
-ggplot(data = data.thi.group)+geom_line(aes(x = 월, y = THI_level_count), group = 1)+geom_point(aes(x = 월, y = THI_level_count), size = 3)
+ggplot(data = data.thi.group)+geom_line(aes(x = 월, y = THI_level_count), group = 1)+
+  geom_point(aes(x = 월, y = THI_level_count), size = 3)
 
 
 
